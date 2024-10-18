c119b536f77367322a666fa612f6ef5dfa5faf8b
Saqibkhan15 <aqib12u@gmail.com>
2024-10-18 11:48:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+// function add(a,b){
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// }
+
+// add(5,5)
+
+
+// function currentDate(){
+//     document.write(new Date());
+//  }
+// currentDate()
+
+
+// function fullname(){
+// var firstName = prompt();
+// var lastName=prompt();
+// alert(firstName+lastName)
+// }
+ 
+// fullname()
+
+// function addition(){
+// var num1 = prompt();
+// var num2=prompt();
+// alert(num1+num2)
+// }
+ 
+// addition()
+
+
